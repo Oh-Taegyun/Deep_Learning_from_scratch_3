f58495e62878f5f0c2963ca749e32d39cea97c1a
luili0307 <luili0307@naver.com>
2022-10-18 15:32:10 +0900

    Graphviz를 활용하여 계산 그래프의 시각화

--- steps/Test.py
+++ steps/Test.py
@@ -1,9 +1,18 @@
-# -------------------------------------------------------------------------------------------------
-# 실행하고자 파는 파일에 주석을 풀면 파이썬 명령어를 어디에서든 실행하던 dezero 디렉터이릐 파일을 임포트할 수 있다.
-# -------------------------------------------------------------------------------------------------
+import numpy as np
+from Dezore import Variable
+from Dezore.utils import plot_dot_graph
 
+def goldstein(x, y):
+    z = (1 + (x+y+1)**2 * (19-14*x + 3*x**2 - 14*y + 6*x*y + 3*y**2)) * (30 + (2*x - 3*y)**2 * (18 - 32*x + 12*x**2 + 48*y - 36*x*y + 27*y**2))
+    return z
 
-if '__file__' in globals():
-    import os, sys
-    sys.path.append(os.path.join(os.path.dirname(__file__),'..'))
+x = Variable(np.array(1.0))
+y = Variable(np.array(1.0))
+z = goldstein(x,y)
+z.backward()
 
+x.name = 'x'
+y.name = 'y'
+z.name = 'z'
+
+plot_dot_graph(z,verbose = False, to_file='goldstein.png')
